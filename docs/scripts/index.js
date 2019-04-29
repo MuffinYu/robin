1b419ee642f0eda60a08b428b8a1008b40650911
喻淞彬 <songbin.yu@tongdun.cn>
2019-04-29 19:57:11 +0800

    service-work 位置调整

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -165,7 +165,7 @@
     }); // TODO add service worker code here
 
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./service-worker.js').then(function () {
+      navigator.serviceWorker.register('service-worker.js').then(function () {
         console.log('Service Worker Registered');
       });
     }
